54b612b6fd435ebb779de2836ea5b22389de78ca
John Frimmel <jrfrimme@unca.edu>
2015-03-24 19:11:35 +0000

    updates theme

--- html/sites/all/themes/lsfbootstrap/template.php
+++ html/sites/all/themes/lsfbootstrap/template.php
@@ -4,3 +4,14 @@
  * @file
  * template.php
  */
+/**
+ * Implements hook_preprocess_region().
+ */
+function lsfbootstrap_preprocess_region(&$variables) {
+  switch ($variables['region']) {
+    case 'sub_footer':
+    case 'footer':
+      $variables['classes_array'][] = 'row';
+      break;
+  }
+}
--- html/sites/all/themes/lsfbootstrap/templates/page.tpl.php
+++ html/sites/all/themes/lsfbootstrap/templates/page.tpl.php
@@ -150,11 +150,6 @@
       <?php endif; ?>
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
@@ -176,6 +171,8 @@
 
   </div>
 </div>
+
 <footer class="footer container">
+  <?php print render($page['sub_footer']); ?>
   <?php print render($page['footer']); ?>
 </footer>
