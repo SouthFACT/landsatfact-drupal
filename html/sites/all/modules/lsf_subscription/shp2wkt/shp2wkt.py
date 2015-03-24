c5144df5ca60a4b877915ad397a1f68a6d37765c
John Frimmel <jrfrimme@unca.edu>
2015-03-24 19:11:14 +0000

    adds new modules

---
+++ html/sites/all/modules/lsf_subscription/shp2wkt/shp2wkt.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+from osgeo import ogr
+import os, sys
+
+shapefile = "CherokeeCounty.shp"
+driver = ogr.GetDriverByName("ESRI Shapefile")
+dataSource = driver.Open(shapefile, 0)
+layer = dataSource.GetLayer()
+
+for feature in layer:
+    geom = feature.GetGeometryRef()
+    x = geom.GetEnvelope()
+
+print x 
