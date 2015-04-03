c2892b57a6b776e83dcdbe3fc8ffa965cbea75b3
Derek Morgan <geoderek@git>
2015-04-03 19:13:26 +0000

    progress on specify AOIs by Upload ShapeFile issue #3

--- html/sites/all/modules/lsf_subscription/shp2wkt/shp2wkt.py
+++ html/sites/all/modules/lsf_subscription/shp2wkt/shp2wkt.py
@@ -1,14 +1,37 @@
 #! /usr/bin/python
+# Example of usage:
+# ./shp2wkt.py "../../../../default/files/shapefiles/CherokeeCounty_1.zip"
 from osgeo import ogr
-import os, sys
+import os, sys, zipfile
 
-shapefile = "CherokeeCounty.shp"
+# print sys.argv[1]
+fileName = os.path.basename(sys.argv[1])
+fileName = os.path.splitext(fileName)[0]
+# print fileName
+
+zf = zipfile.ZipFile(sys.argv[1], 'r')
+# Get the three mandatory files of .shp, .shx, and .dbf
+sourceZip = zipfile.ZipFile(sys.argv[1], 'r')
+for filename in zf.namelist():
+	print os.path.splitext(filename)[1]
+	if os.path.splitext(filename)[1]=='.shp':
+		print "doing shp file"
+		shpFile = filename
+		sourceZip.extract(shpFile,os.getcwd())
+	if os.path.splitext(filename)[1]=='.shx':
+		print "doing shx file"
+		shxFile = filename
+		sourceZip.extract(shxFile,os.getcwd())
+	if os.path.splitext(filename)[1]=='.dbf':
+		print "doing dbf file"
+		dbfFile = filename
+		sourceZip.extract(dbfFile,os.getcwd())		
+
 driver = ogr.GetDriverByName("ESRI Shapefile")
 dataSource = driver.Open(shapefile, 0)
 layer = dataSource.GetLayer()
 
 for feature in layer:
     geom = feature.GetGeometryRef()
-    x = geom.GetEnvelope()
-
-print x 
+    wkt = geom.GetEnvelope()
+print wkt 
