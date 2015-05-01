fa5d7def72b392ee5fa7827f1c3250fd8b5ae16c
Derek Morgan <geoderek@git>
2015-05-01 16:56:26 +0000

    update git ignore and geojson script

--- html/sites/all/modules/lsf_subscription/populate_geojson.js
+++ html/sites/all/modules/lsf_subscription/populate_geojson.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function populate_geojson() { 
 	var geojson = Drupal.settings.lsf_subscription.geojson;
-	jQuery("#edit-field-area-geojson-und-0-geom").val(geojson);	
+	jQuery("#edit-field-area-geojson-und-0-geom").val(geojson);
+    jQuery('#edit-field-area-geojson-und-0-geom').attr('readonly', 'true'); // mark it as read only
+    jQuery('#edit-field-area-geojson-und-0-geom').css('background-color' , '#DEDEDE'); // change the background color	
 });
