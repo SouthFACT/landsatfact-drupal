052547da0db217625b6d5a74507fd0ab1922eb31
John Frimmel <jrfrimme@unca.edu>
2015-06-23 12:48:08 +0000

    fixes issues in the lsf_splash module

--- html/sites/all/modules/lsf_splash/lsf_splash.js
+++ html/sites/all/modules/lsf_splash/lsf_splash.js
@@ -2,6 +2,11 @@
     var width = 500;
     var height = 600;
     var ratio = 1.2;
+    var autoOpen = false;
+    // If no cookie, then show disclaimer
+    if (!Cookies.get('showdisclaimer') || Cookies.get('showdisclaimer') !== "false") {
+        autoOpen = true;
+    }
 
     if ($(window).width() < 520) {
 	width = $(window).width() - 20;
@@ -16,6 +21,17 @@
         height: height,
         maxWidth: 600,
         maxHeight: 600,
-	draggable: false
+	draggable: false,
+	closeText: 'close',
+	autoOpen : autoOpen
     });
+
+    $("#disclaimer_seen").change(setDisclaimerCookie);
+
+    function setDisclaimerCookie() {
+        if ($(this).prop("checked") === true) {
+            Cookies.set("showdisclaimer", "false", { expires: 365 });
+            $("#disclaimer").dialog("close");
+        }
+    };
 }(jQuery);
