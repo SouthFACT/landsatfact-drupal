8962516a82c2bfb6ba2256b19735aa632ed28341
John Frimmel <jrfrimme@unca.edu>
2017-07-24 19:41:50 +0000

    updates to theme

--- html/sites/all/themes/lsfbootstrap/templates/views-view-field--nid.tpl.php
+++ html/sites/all/themes/lsfbootstrap/templates/views-view-field--nid.tpl.php
@@ -22,59 +22,58 @@
  * the view is modified.
  */
 
-     $node_id = $row->{$view->field['nid']->field_alias};
-     $drupalUser = $row->{$view->field['name']->field_alias};
-     $postDate = $row->{$view->field['changed']->field_alias};
+     if ($view->name === 'custom_request_status') {
+         $node_id = $row->{$view->field['nid']->field_alias};
+         $drupalUser = $row->{$view->field['name']->field_alias};
+         $postDate = $row->{$view->field['changed']->field_alias};
 
-     $config_info = parse_ini_file(DRUPAL_ROOT . '/../lsf_config.ini', true);
-     $username = $config_info['pgsql_connection']['username'];
-     $password = $config_info['pgsql_connection']['password'];
-     $host = $config_info['pgsql_connection']['host'];
-     $port = $config_info['pgsql_connection']['port'];
-     $driver = $config_info['pgsql_connection']['driver'];
-     $database = $config_info['pgsql_connection']['database'];
-     $lsf_database = array(
-         'database' => $database,
-         'username' => $username,
-         'password' => $password,
-         'host' => $host,
-         'port' => $port,
-         'driver' => $driver,
-     );
+         $config_info = parse_ini_file(DRUPAL_ROOT . '/../lsf_config.ini', true);
+         $username = $config_info['pgsql_connection']['username'];
+         $password = $config_info['pgsql_connection']['password'];
+         $host = $config_info['pgsql_connection']['host'];
+         $port = $config_info['pgsql_connection']['port'];
+         $driver = $config_info['pgsql_connection']['driver'];
+         $database = $config_info['pgsql_connection']['database'];
+         $lsf_database = array(
+             'database' => $database,
+             'username' => $username,
+             'password' => $password,
+             'host' => $host,
+             'port' => $port,
+             'driver' => $driver,
+         );
 
-     Database::addConnectionInfo($database, 'default', $lsf_database);
-     db_set_active($database);
+         Database::addConnectionInfo($database, 'default', $lsf_database);
+         db_set_active($database);
 
-     //get the aoi_id for the Custom Request 
-     //   the download files uses the aoi_id in the file name. which is in postgres database
-     //   retrieve to concat the URL
-     if ($view->name === "custom_request_status") {
-            $result = db_query('select * from get_aoi_id_by_nodeid(:nid)',
-                               array(
-                                   ':nid' => $node_id,
-                               ));   
-     }
-
+         //get the aoi_id for the Custom Request 
+         //   the download files uses the aoi_id in the file name. which is in postgres database
+         //   retrieve to concat the URL
+         $result = db_query('select * from get_aoi_id_by_nodeid(:nid)',
+                            array(
+                                ':nid' => $node_id,
+                            ));   
      
-     $aoi_id = 0;
+         $aoi_id = 0;
 
-     //retreive aoi_id from results
-     foreach($result as $item) {
-       $aoi_id = $item->get_aoi_id_by_nodeid;
-       break;
+         //retreive aoi_id from results
+         foreach($result as $item) {
+             $aoi_id = $item->get_aoi_id_by_nodeid;
+             break;
+         }
+
+         if (strlen($aoi_id) > 0){
+             $message = $aoi_id;
+         } else {
+             $message = 'Not Available';
+         }
+
+         db_set_active();
+
+         //$output replaces field with our custom message
+         $output = $message;
      }
 
-     if (strlen($aoi_id) > 0){
-      $message = $aoi_id;
-     } else {
-      $message = 'Not Available';
-     }
-
-     db_set_active();
-
-     //$output replaces field with our custom message
-     $output = $message;
-     
 ?>
 <?php /* dpm( current( (Array)$view->field) );*/   ?>
 <?php  print $output; ?>
